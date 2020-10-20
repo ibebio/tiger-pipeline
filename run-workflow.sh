6cdf68de0e1bd5950dd0e11635d3a77d485bded1
Ilja Bezrukov <ibezrukov@tuebingen.mpg.de>
2020-10-20 18:55:56 +0200

    add local option to workflow driver script

--- run-workflow.sh
+++ run-workflow.sh
@@ -9,6 +9,20 @@ LOCAL_CORES=10
 # Allowed number of parallel cluster jobs
 JOBS=50
 
+
+
+if [[ "$1" != "local" ]] && [[ "$1" != "sge" ]] ; then
+    echo "Usage: $0 local|sge [additional Snakemake arguments]"
+    echo "The first argument is required, and is either the string 'local' or 'sge'."
+    echo "It determines whether the workflow should be run on the local machine (local"
+    echo "or on the SGE cluster (sge)."
+    echo "Any additional aguments are passed on to Snakemake"
+    exit 1
+fi
+
+SNAKEMAKE_PROFILE=$1
+shift
+
 # Any additional arguments to the script are passed on to Snakemake
 # e.g. --forceall would force to rerun all rules and recreate all
 #files.
@@ -26,9 +40,18 @@ if [[ ! -d ${PIPELINE_DIR}/conda/ ]] ; then
 fi
 
 # Run the workflow
-snakemake \
-    --local-cores ${LOCAL_CORES} \
-    --jobs ${JOBS} \
-    --use-conda \
-    --profile sge \
-    ${SNAKEMAKE_ARGS}
+
+if [[ "${SNAKEMAKE_PROFILE}" == "sge" ]] ; then
+    snakemake \
+	--local-cores ${LOCAL_CORES} \
+	--jobs ${JOBS} \
+	--use-conda \
+	--profile sge \
+	${SNAKEMAKE_ARGS}
+else
+    snakemake \
+	--jobs ${LOCAL_CORES} \
+	--use-conda \
+	${SNAKEMAKE_ARGS}
+fi
+
